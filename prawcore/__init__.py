1c38fd0206591b1d0ca407d256196aa9c69cea42
Bryce Boe <bbzbryce@gmail.com>
2016-02-06 15:29:58 -0800

    Move constants into prawcore.const.

--- prawcore/__init__.py
+++ prawcore/__init__.py
@@ -1,10 +1,8 @@
 """prawcore: Low-level communication layer for PRAW 4+."""
 
 import logging
+from .const import __version__  # noqa
 from .sessions import Session, session  # noqa
-
-
-__version__ = '0.0.1a1'
 
 
 logging.getLogger(__package__).addHandler(logging.NullHandler())
