dc045a726a7f433de075e472486f1142134c94d8
Bryce Boe <bbzbryce@gmail.com>
2016-02-06 16:23:39 -0800

    Handle BASIC_AUTH placeholder issue with base64 encode types.

--- tests/config.py
+++ tests/config.py
@@ -10,8 +10,12 @@ CLIENT_SECRET = os.environ.get('PRAWCORE_CLIENT_SECRET', 'fake_client_secret')
 REFRESH_TOKEN = os.environ.get('PRAWCORE_REFRESH_TOKEN', 'fake_refresh_token')
 
 
-Betamax.register_serializer(pretty_json.PrettyJSONSerializer)
+def b64_string(input_string):
+    """Return a base64 encoded string (not bytes) from input_string."""
+    return b64encode(input_string.encode('utf-8')).decode('utf-8')
 
+
+Betamax.register_serializer(pretty_json.PrettyJSONSerializer)
 
 with Betamax.configure() as config:
     if os.getenv('TRAVIS'):
@@ -21,5 +25,5 @@ with Betamax.configure() as config:
     config.define_cassette_placeholder('<CLIENT_ID>', CLIENT_ID)
     config.define_cassette_placeholder('<CLIENT_SECRET>', CLIENT_SECRET)
     config.define_cassette_placeholder('<REFRESH_TOKEN>', REFRESH_TOKEN)
-    config.define_cassette_placeholder('<BASIC_AUTH>', b64encode(
+    config.define_cassette_placeholder('<BASIC_AUTH>', b64_string(
         '{}:{}'.format(CLIENT_ID, CLIENT_SECRET)))
