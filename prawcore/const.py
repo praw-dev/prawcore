3c1ee31169a237c6a025505a6b6e19f42ca0813c
Bryce Boe <bbzbryce@gmail.com>
2016-02-14 00:38:06 -0800

    Add authorize_url method to Authenticator class.

--- prawcore/const.py
+++ prawcore/const.py
@@ -3,5 +3,6 @@
 __version__ = '0.0.1a1'
 
 ACCESS_TOKEN_URL = 'https://www.reddit.com/api/v1/access_token'
+AUTHORIZATION_URL = 'https://www.reddit.com/api/v1/authorize'
 
 USER_AGENT = 'prawcore/{}'.format(__version__)
