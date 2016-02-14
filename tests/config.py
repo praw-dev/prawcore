04e91cbfd0bb7403a75e53b31b9ac7eba138ecf1
Bryce Boe <bbzbryce@gmail.com>
2016-02-13 20:21:10 -0800

    Add tests for ScriptAuthorizer.

--- tests/config.py
+++ tests/config.py
@@ -7,7 +7,9 @@ from betamax_serializers import pretty_json
 
 CLIENT_ID = os.environ.get('PRAWCORE_CLIENT_ID', 'fake_client_id')
 CLIENT_SECRET = os.environ.get('PRAWCORE_CLIENT_SECRET', 'fake_client_secret')
+PASSWORD = os.environ.get('PRAWCORE_PASSWORD', 'fake_password')
 REFRESH_TOKEN = os.environ.get('PRAWCORE_REFRESH_TOKEN', 'fake_refresh_token')
+USERNAME = os.environ.get('PRAWCORE_USERNAME', 'fake_username')
 
 
 def b64_string(input_string):
@@ -22,8 +24,10 @@ with Betamax.configure() as config:
         config.default_cassette_options['record_mode'] = 'none'
     config.cassette_library_dir = 'tests/cassettes'
     config.default_cassette_options['serialize_with'] = 'prettyjson'
-    config.define_cassette_placeholder('<CLIENT_ID>', CLIENT_ID)
-    config.define_cassette_placeholder('<CLIENT_SECRET>', CLIENT_SECRET)
-    config.define_cassette_placeholder('<REFRESH_TOKEN>', REFRESH_TOKEN)
     config.define_cassette_placeholder('<BASIC_AUTH>', b64_string(
         '{}:{}'.format(CLIENT_ID, CLIENT_SECRET)))
+    config.define_cassette_placeholder('<CLIENT_ID>', CLIENT_ID)
+    config.define_cassette_placeholder('<CLIENT_SECRET>', CLIENT_SECRET)
+    config.define_cassette_placeholder('<PASSWORD>', PASSWORD)
+    config.define_cassette_placeholder('<REFRESH_TOKEN>', REFRESH_TOKEN)
+    config.define_cassette_placeholder('<USERNAME>', USERNAME)
