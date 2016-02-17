4f5f9bffa85a3294981f25e4e6cdcfb9157b157b
Bryce Boe <bbzbryce@gmail.com>
2016-02-16 21:31:48 -0800

    Require Authorizer instance with session. Fix Session related tests.

--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -1,83 +1,87 @@
 """Test for prawcore.Sessions module."""
 import prawcore
 import unittest
-from .config import CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN
+from .config import CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN, REQUESTOR
 from betamax import Betamax
+from prawcore.auth import Authorizer
 
 
-class InvalidAuthorizer(object):
+class InvalidAuthorizer(Authorizer):
     def is_valid(self):
         return False
 
 
-def readonly_authorizer():
-    authenticator = prawcore.Authenticator(CLIENT_ID, CLIENT_SECRET)
+def readonly_authorizer(refresh=True):
+    authenticator = prawcore.Authenticator(CLIENT_ID, CLIENT_SECRET,
+                                           requestor=REQUESTOR)
     authorizer = prawcore.ReadOnlyAuthorizer(authenticator)
-    authorizer.refresh()
+    if refresh:
+        authorizer.refresh()
     return authorizer
 
 
+def simple_session():
+    return prawcore.Session(readonly_authorizer(refresh=False))
+
+
 def valid_authorizer():
-    authenticator = prawcore.Authenticator(CLIENT_ID, CLIENT_SECRET)
+    authenticator = prawcore.Authenticator(CLIENT_ID, CLIENT_SECRET,
+                                           requestor=REQUESTOR)
     authorizer = prawcore.Authorizer(authenticator, REFRESH_TOKEN)
     authorizer.refresh()
     return authorizer
 
 
 class SessionTest(unittest.TestCase):
-    def setUp(self):
-        self.session = prawcore.Session()
-
     def test_close(self):
-        self.session.close()
+        prawcore.Session(readonly_authorizer(refresh=False)).close()
 
     def test_context_manager(self):
-        with self.session as session:
+        with prawcore.Session(readonly_authorizer(refresh=False)) as session:
             self.assertIsInstance(session, prawcore.Session)
 
+    def test_init__without_authenticator(self):
+        self.assertRaises(prawcore.InvalidInvocation, prawcore.Session, None)
+
     def test_request(self):
-        with Betamax(prawcore.util.http).use_cassette(
-                'Session_request'):
-            self.session.authorizer = readonly_authorizer()
-            data = self.session.request('GET', 'https://oauth.reddit.com')
+        with Betamax(REQUESTOR).use_cassette('Session_request'):
+            session = prawcore.Session(readonly_authorizer())
+            data = session.request('GET', 'https://oauth.reddit.com')
         self.assertIsInstance(data, dict)
         self.assertEqual('Listing', data['kind'])
 
     def test_request__raw_json(self):
-        with Betamax(prawcore.util.http).use_cassette(
+        with Betamax(REQUESTOR).use_cassette(
                 'Session_request__raw_json'):
-            self.session.authorizer = readonly_authorizer()
-            data = self.session.request(
+            session = prawcore.Session(readonly_authorizer())
+            data = session.request(
                 'GET', ('https://oauth.reddit.com/r/reddit'
                         '_api_test/comments/45xjdr/want_raw_json_test/'))
         self.assertEqual('WANT_RAW_JSON test: < > &',
                          data[0]['data']['children'][0]['data']['title'])
 
     def test_request__with_insufficent_scope(self):
-        with Betamax(prawcore.util.http).use_cassette(
+        with Betamax(REQUESTOR).use_cassette(
                 'Session_request__with_insufficient_scope'):
-            self.session.authorizer = valid_authorizer()
-            self.assertRaises(prawcore.InsufficientScope, self.session.request,
+            session = prawcore.Session(valid_authorizer())
+            self.assertRaises(prawcore.InsufficientScope, session.request,
                               'GET', 'https://oauth.reddit.com/api/v1/me')
 
     def test_request__with_invalid_access_token(self):
-        with Betamax(prawcore.util.http).use_cassette(
+        with Betamax(REQUESTOR).use_cassette(
                 'Session_request__with_invalid_access_token'):
-            self.session.authorizer = readonly_authorizer()
-            self.session.authorizer.access_token += 'invalid'
-            self.assertRaises(prawcore.InvalidToken, self.session.request,
-                              'GET', 'https://oauth.reddit.com')
+            session = prawcore.Session(readonly_authorizer())
+            session._authorizer.access_token += 'invalid'
+            self.assertRaises(prawcore.InvalidToken, session.request, 'GET',
+                              'https://oauth.reddit.com')
 
     def test_request__with_invalid_authorizer(self):
-        self.session.authorizer = InvalidAuthorizer()
-        self.assertRaises(prawcore.InvalidInvocation, self.session.request,
-                          'get', '')
-
-    def test_request__without_authorizer(self):
-        self.assertRaises(prawcore.InvalidInvocation, self.session.request,
+        session = prawcore.Session(InvalidAuthorizer(None))
+        self.assertRaises(prawcore.InvalidInvocation, session.request,
                           'get', '')
 
 
 class SessionFunctionTest(unittest.TestCase):
     def test_session(self):
-        self.assertIsInstance(prawcore.session(), prawcore.Session)
+        self.assertIsInstance(prawcore.session(InvalidAuthorizer(None)),
+                              prawcore.Session)
