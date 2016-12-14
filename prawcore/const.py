1a25c2efd81cc964bd5248e7f80aab0cc37750af
Bryce Boe <bbzbryce@gmail.com>
2016-12-13 21:50:47 -0800

    Bump to 0.5.0

--- prawcore/const.py
+++ prawcore/const.py
@@ -1,7 +1,7 @@
 """Constants for the prawcore package."""
 import os
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 
 ACCESS_TOKEN_PATH = '/api/v1/access_token'
 AUTHORIZATION_PATH = '/api/v1/authorize'
