6ce847ce4dfc26c8a6f85a9f251c70da670a8b94
Bryce Boe <bbzbryce@gmail.com>
2017-01-15 12:04:18 -0800

    Support python 3.6

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(name=PACKAGE_NAME,
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: Implementation :: CPython'],
       dependency_links=[
           ('https://github.com/sigmavirus24/betamax_matchers/tarball/master'
