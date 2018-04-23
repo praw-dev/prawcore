bdb3c79abf584a536d90d156437a6e37538e1cf8
epiphyte <epiphyte@vertex.link>
2018-04-23 09:17:56 -0400

    Log sleep calls done in the rate limiter.

--- prawcore/rate_limit.py
+++ prawcore/rate_limit.py
@@ -1,5 +1,8 @@
 """Provide the RateLimiter class."""
 import time
+import logging
+
+log = logging.getLogger(__package__)
 
 
 class RateLimiter(object):
@@ -41,6 +44,7 @@ class RateLimiter(object):
         sleep_seconds = self.next_request_timestamp - time.time()
         if sleep_seconds <= 0:
             return
+        log.debug('Sleeping: {:0.2f} seconds'.format(sleep_seconds))
         time.sleep(sleep_seconds)
 
     def update(self, response_headers):
