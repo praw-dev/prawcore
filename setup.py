bb0e001e5064af481baca77ce8c142306a725310
Bryce Boe <bbzbryce@gmail.com>
2020-01-04 23:19:22 -0800

    Add classifier for python 3.8

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: 3.8",
     ],
     description="Low-level communication layer for PRAW 4+.",
     extras_require={"dev": ["pre-commit"]},
